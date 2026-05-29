24d5a76b9a443f2df38ea715bf52684d1aed494e
himanshu17113 <98476250+himanshu17113@users.noreply.github.com>
2026-05-29 16:04:57 +0530

    ADDED PATCH

--- api/proxy.js
+++ api/proxy.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', '*');
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
